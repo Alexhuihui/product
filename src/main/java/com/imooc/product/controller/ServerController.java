36d6fff81102c57d2a644b26c0544519c51659d7
wangyonghui <2930807240@qq.com>
2019-03-01 18:18:19 +0800

    测试应用通信

---
+++ src/main/java/com/imooc/product/controller/ServerController.java
@@ -0,0 +1,16 @@
+package com.imooc.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 汪永晖
+ */
+@RestController
+public class ServerController {
+
+    @GetMapping("/msg")
+    public String msg() {
+        return "this is product msg";
+    }
+}
